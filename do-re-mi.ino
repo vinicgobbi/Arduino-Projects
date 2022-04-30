e1992117a8465976a4ed01bb6a22004b9906af54
Viniciuscgobbi <vinicius.cgobbi2004@gmail.com>
2022-04-30 12:09:35 -0300

    Commit principal

---
+++ do-re-mi.ino
@@ -0,0 +1,100 @@
+// C++ code
+//
+
+////////////////////////////
+//    Parte Organizada    //
+////////////////////////////
+
+// Create vars
+
+const int Do = 261; //Dó
+const int Re = 293; //Ré
+const int Mi = 329; //Mí
+const int Fa = 349; //Fá
+const int Sol = 391; //Sol
+const int La = 440; //Lá
+const int Si = 493; //Sí
+
+void setup()
+{
+  //Set pin-mode
+  pinMode(10, OUTPUT);
+}
+
+/////////////////////////
+//  Parte Bagunçada    //
+/////////////////////////
+void loop()
+{
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Fa);
+  delay(500);
+  noTone(10);
+  delay(300);
+  tone(10, Fa);
+  delay(300);
+  noTone(10);
+  delay(300);
+  tone(10, Fa);
+  delay(500);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(300);
+  noTone(10);
+  delay(300);
+  tone(10, Re);
+  delay(300);
+  noTone(10);
+  delay(300);
+  tone(10, Re);
+  delay(300);
+  tone(10, Do);
+  delay(500);
+  tone(10, Sol);
+  delay(500);
+  tone(10, Fa);
+  delay(500);
+  tone(10, Mi);
+  delay(300);
+  noTone(10);
+  delay(300);
+  tone(10, Mi);
+  delay(300);
+  noTone(10);
+  delay(300);
+  tone(10, Mi);
+  delay(300);
+  noTone(10);
+  delay(500);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Fa);
+  delay(500);
+  noTone(10);
+  delay(300);
+  tone(10, Fa);
+  delay(300);
+  noTone(10);
+  delay(300);
+  tone(10, Fa);
+  delay(500);
+  noTone(10);
+  delay(5000);
+}
+
+//PS.: Se reclamarem da bagunça, o Autor é anonimo!!
+//PS. 2: Se estiver bom, foi o Vinícius que fez!!
---
+++ nona-sinfonia.ino
@@ -0,0 +1,226 @@
+// C++ code
+//
+
+////////////////////////////
+//    Parte Organizada    //
+////////////////////////////
+
+// Create vars
+
+const int Do = 261; //Dó
+const int Re = 293; //Ré
+const int Mi = 329; //Mí
+const int Fa = 349; //Fá
+const int Sol = 391; //Sol
+const int La = 440; //Lá
+const int Si = 493; //Sí
+
+void setup()
+{
+  //Set pin-mode
+  pinMode(10, OUTPUT);
+}
+
+/////////////////////////
+//  Parte Bagunçada    //
+/////////////////////////
+void loop()
+{
+  tone(10, Mi);
+  delay(200);
+  noTone(10);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Fa);
+  delay(500);
+  tone(10, Sol);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Sol);
+  delay(500);
+  tone(10, Fa);
+  delay(500);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Do);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Re);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Re);
+  delay(800);
+  noTone(10);
+  tone(10, Mi);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Fa);
+  delay(500);
+  tone(10, Sol);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Sol);
+  delay(500);
+  tone(10, Fa);
+  delay(500);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Do);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Do);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Do);
+  delay(200);
+  noTone(10);
+  delay(800);
+  tone(10, Re);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(300);
+  tone(10, Fa);
+  delay(300);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(300);
+  tone(10, Fa);
+  delay(300);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Sol);
+  delay(500);
+  tone(10, Re);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(300);
+  tone(10, Fa);
+  delay(300);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(300);
+  tone(10, Fa);
+  delay(300);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Sol);
+  delay(500);
+  tone(10, Mi);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Fa);
+  delay(500);
+  tone(10, Sol);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Sol);
+  delay(500);
+  tone(10, Fa);
+  delay(500);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Do);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Do);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Mi);
+  delay(500);
+  tone(10, Re);
+  delay(500);
+  tone(10, Do);
+  delay(200);
+  noTone(10);
+  delay(100);
+  tone(10, Do);
+  delay(200);
+  noTone(10);
+  delay(5000);
+
+
+}
+
+//PS.: Se reclamarem da bagunça, o Autor é anonimo!!
+//PS. 2: Se estiver bom, foi o Vinícius que fez!!
